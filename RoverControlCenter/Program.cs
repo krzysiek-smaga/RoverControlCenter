31156765d8227a9fb7b08427b856b5cb6eae2cd1
Krzysiek Smaga <krzysiek.at@gmail.com>
2020-11-15 16:40:58 +0100

    Add project files.

---
+++ RoverControlCenter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverControlCenter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                MoonRover lunokhod = new MoonRover("Lunokhod 1", 1970);
+                MoonRover apollo = new MoonRover("Apollo 15", 1971);
+                MarsRover sojourner = new MarsRover("Sojourner", 1997);
+                Satellite sputnik = new Satellite("Sputnik", 1957);
+
+
+
+                Console.ReadLine();
+            }
+    }
+}
